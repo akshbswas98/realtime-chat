024a922b03cf76f0fecba8c41a9fd9441cdc6de6
Akash Biswas <64808720+akshbswas98@users.noreply.github.com>
2021-09-30 12:37:28 +0530

    A Simpe realtime react chat app using the timeline view

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "primereact/resources/themes/saga-purple/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import React, { useState } from "react";
+import { InputText } from "primereact/inputtext";
+import { io } from "socket.io-client"
+import { Button } from "primereact/button"
+import { Timeline } from "primereact/timeline"
+const socket = io("http://localhost:3001");
 function App() {
+  const [author, setAuthor] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([
+    { author: "Bot", message: "Hello!" },
+  ]);
+  socket.on("chat", (arg) => setMessages([...messages,arg]))
+  const addMessage = () => {
+    const send = {author, message };
+    setMessages([...messages,send]);
+    setMessage("");
+    socket.emit("chat",send);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div>
+      <div className="p-col-12">
+       <div className="p-inputgroup">
+        <span className="p-inputgroup-addon">
+        <i className="pi pi-user"></i>
+        </span>
+        <InputText placeholder="Name" value={author} onChange={(e) => setAuthor(e.target.value)} />
+        <InputText placeholder="Message"  value={message} onChange={(e) => setMessage(e.target.value)} />
+        <Button label="Send" onClick={() => addMessage() }/>
+      </div>
+      <div className="card">
+           <h5> Conversation </h5>
+           <Timeline value={messages} opposite={(item) => item.author } 
+           content={(item) => (<small className="p-text-secondary">{item.message}</small>)} />
+           
+           </div>
+     </div>
     </div>
-  );
+  )
 }
 
 export default App;
